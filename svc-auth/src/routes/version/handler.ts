3c16fa5c11f7c75f71927e9f3b25cd6a0e1721bb
Oleksii Honchar <tuiteraz@gmail.com>
2019-09-13 18:28:00 +0300

    fix: lint fix

--- svc-auth/src/routes/version/handler.ts
+++ svc-auth/src/routes/version/handler.ts
@@ -6,7 +6,7 @@ import * as pkg from 'package.json';
 function get (req: Request, res: Response, next: NextFunction) {
   const packageName = pkg.name || 'not specified';
   const packageVersion = pkg.version || 'not specified';
-  res['body'] = `${packageName}:${packageVersion}`;
+  res.body = `${packageName}:${packageVersion}`;
   res.statusCode = HttpStatusCodes.OK;
   next();
 }
